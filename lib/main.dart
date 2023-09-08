7a59c9ec9869e8c0dbc52d06412769c9a44d8d78
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2023-09-07 21:37:55 -0300

    3Device Preview Package - Teste o App em quaisquer dispositivo

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:academia_flutter_flutter/pages/container/container_page.dart';
 import 'package:academia_flutter_flutter/pages/home/home_page.dart';
 import 'package:academia_flutter_flutter/pages/midia_quary/media_query._page.dart';
 import 'package:academia_flutter_flutter/pages/rows_collumns/rows_columns.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: false ,//!kReleaseMode,
+    builder: (_) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,17 +23,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Academia do flutter',
-      // theme: ThemeData(       
+      // theme: ThemeData(
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
+       locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, 
       routes: {
-        HomePage.routName:      (_) => HomePage(),
-        ContainerPage.routName :(_) => ContainerPage(),
-        RowsColumns.routName:   (_) => RowsColumns(),
-        MediaQueryPage.routName:(_) => MediaQueryPage()
+        HomePage.routName: (_) => HomePage(),
+        ContainerPage.routName: (_) => ContainerPage(),
+        RowsColumns.routName: (_) => RowsColumns(),
+        MediaQueryPage.routName: (_) => MediaQueryPage()
       },
     );
   }
 }
-
